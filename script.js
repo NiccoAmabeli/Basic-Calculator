8a63dcdd994a854d7d6653d7c635fca71ba4efa7
Amabeli_Nicco <Amabeli_Nicco@student.MahoningCTC.com>
2021-01-27 08:13:20 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById("pageTitle").innerHTML = "Mr.Sekol Coding Calculator";
+
+function mathAddition(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat (numbOne + numbTwo);
+};
+function mathSubtraction(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat (numbOne - numbTwo);
+};
+function mathMultiplication(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat (numbOne * numbTwo);
+};
+function mathDivision(){
+    let numbOne = parseFloat(document.getElementById("numberOne").value);
+    let numbTwo = parseFloat(document.getElementById("numberTwo").value);
+    document.getElementById("correctAnswer").innerHTML = "The Correct Answer Is: " + parseFloat (numbOne / numbTwo);
+};
